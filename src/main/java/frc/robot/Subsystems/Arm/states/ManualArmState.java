71b19ed84bd7613eea7edc5c7c363f1a5c25c7c1
jrubacha <jrubacha@lsc.k12.in.us>
2023-02-15 12:00:42 -0500

    manual arm control

--- src/main/java/frc/robot/Subsystems/Arm/states/ManualArmState.java
+++ src/main/java/frc/robot/Subsystems/Arm/states/ManualArmState.java
@@ -6,6 +6,7 @@ package frc.robot.Subsystems.Arm.states;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Subsystems.Arm.ArmSubsystem;
+import frc.robot.constants.Constants;
 import frc.robot.lib.Controllers;
 
 public class ManualArmState extends CommandBase {
@@ -24,6 +25,32 @@ public class ManualArmState extends CommandBase {
       power *= 3;
     }
     arm.setShoulderPower(power);
+
+
+    // Shoulder + Elbow Values
+    double shoulderPower = -0.5 * Controllers.getOperatorController().getRawAxis(Controllers.PS4_Controller.Axis.LEFT_STICK_Y);
+    double elbowPower = -0.5 * Controllers.getOperatorController().getRawAxis(Controllers.PS4_Controller.Axis.LEFT_STICK_X);
+
+    // Shoulder Control
+    if (shoulderPower > 0.0 && arm.getShoulderPosition() > Constants.ArmConstants.SHOULDER_HIGH_LIMIT) { // Arm too high
+      arm.setShoulderPower(0);
+    } else if (shoulderPower < 0.0 && arm.getShoulderPosition() < Constants.ArmConstants.SHOULDER_LOW_LIMIT) { // Arm too low
+      arm.setShoulderPower(0);
+    } else {
+      arm.setShoulderPower(shoulderPower);
+    }
+
+    // Elbow Control
+    if (elbowPower > 0.0 && arm.getElbowPosition() > Constants.ArmConstants.ELBOW_HIGH_LIMIT) {
+      arm.setElbowPower(0);
+    } else if (elbowPower < 0.0 && arm.getElbowPosition() < Constants.ArmConstants.ELBOW_LOW_LIMIT) {
+      arm.setElbowPower(0);
+    } else {
+      arm.setElbowPower(elbowPower);
+    }
+
+    // NOTE: These are worst case value limits. Given the arm geometry and many ways it needs to fold, this will not stop crashes. That'll need math.
+
   }
 
 }
